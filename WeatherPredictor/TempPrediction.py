0113c9215976e08f37e117898adda18555f670d9
gte999s <nikolas.d@gmail.com>
2017-06-06 00:05:04 -0400

    size issue

--- WeatherPredictor/TempPrediction.py
+++ WeatherPredictor/TempPrediction.py
@@ -18,11 +18,11 @@ class tempModel(object):
     Input consists of [temp humidity dewpoint pressure] * number of stations
     """
     def __init__(self, num_of_classes,  
-                       truncated_backprop = 7*24,  isTraining = True, 
+                       truncated_backprop_num = 7*24,  isTraining = True, 
                        batch_size = 1000, stations = [3001, 3002, 3003], state_size = 10,
                        dropout_keep_prob = .9, num_rnn_layers = 10):
         self.isTraining = isTraining
-        self.truncated_backprop = truncated_backprop
+        self.truncated_backprop_num = truncated_backprop_num
         self.batch_size = batch_size
         self.inputLength = 4 * len(stations)
         self.state_size = state_size
@@ -31,7 +31,7 @@ class tempModel(object):
         self.num_of_classes = num_of_classes
         self.stations = stations
 
-        if self.isTraining:
+        if not self.isTraining:
             self.batch_size = 1
         # Build model
         self._buildModel()
@@ -49,31 +49,39 @@ class tempModel(object):
                                                           1],
                                              name='target_batch')
             if self.isTraining:
-                target_batch_cur = target_batch_PM[:,-24:,:] # Take only the last 24 hours of results for cost calc
-                target_batch_cur = tf.one_hot(target_batch_cur, depth=self.num_of_classes)
+                target_batch_cur = target_batch_PH[:,-24:,:] # Take only the last 24 hours of results for cost calc
+                target_batch_cur = tf.one_hot(tf.to_int32(target_batch_cur), depth=self.num_of_classes)
             else:
-                target_batch_cur = tf.one_hot(target_batch_cur, depth=self.num_of_classes) # Take everything when running predictions
+                target_batch_cur = tf.one_hot(target_batch_PH, depth=self.num_of_classes) # Take everything when running predictions
             
             # LSTM / Dynamic RNN
         with tf.name_scope('LSTM'):
             doReuse = not self.isTraining
-            cell = tf.contrib.rnn.BasicLSTMCell(self.state_size, state_is_tuple=True, reuse=doReuse)
+            cells=[]
             if self.isTraining:
-                cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=self.dropout_keep_prob)
-            cell = tf.nn.rnn_cell.MultiRNNCell([cell] * self.num_rnn_layers, state_is_tuple=True)
+                for _ in range(self.num_rnn_layers):
+                    cell = tf.contrib.rnn.BasicLSTMCell(self.state_size, state_is_tuple=True, reuse=doReuse)
+                    cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=self.dropout_keep_prob)
+                    cells.append(cell)
+            else:
+                for _ in range(self.num_rnn_layers):
+                    cell = tf.contrib.rnn.BasicLSTMCell(self.state_size, state_is_tuple=True, reuse=doReuse)
+                
+            cell = tf.contrib.rnn.MultiRNNCell(cells, state_is_tuple=True)
             outputs_batch, _ = tf.nn.dynamic_rnn(cell, input_batch_PH, dtype=tf.float32)
             outputs = tf.reshape(outputs_batch,[-1, self.state_size])
 
         with tf.name_scope('LinearLayer'):
-            W = tf.get_variable('W',shape=(self.state_size, self.num_of_classes),dtype=tf.float32)
+            W = tf.get_variable('W', shape=(self.state_size, self.num_of_classes),dtype=tf.float32)
             b = tf.get_variable('b',shape=(1,self.num_of_classes))
             temp_pred_logits = tf.matmul(outputs, W) + b
-            self.temp_pred_prob = tf.sparse_softmax(temp_pred_logits)
+            self.temp_pred_prob = tf.nn.softmax(temp_pred_logits)
 
         if self.isTraining:
 
             with tf.name_scope('cost_calc'):
-                costs = tf.nn.sparse_softmax_cross_entropy_with_logits(temp_pred_logits,target_batch_cur)
+                costs = tf.nn.softmax_cross_entropy_with_logits(logits=tf.reshape(temp_pred_logits,(self.batch_size,self.truncated_backprop_num,self.num_of_classes))[:,-24:,:],
+                                                                labels=target_batch_cur)
                 self.cost_mean = tf.reduce_mean(costs)
 
 
@@ -90,9 +98,9 @@ if __name__ == "__main__":
     min_temp = -30
     max_temp = 120
     num_of_classes = len(range(min_temp,max_temp)) # model temperatures for -30 F to 120 F
-    num_of_rnn_layers = 4
+    num_rnn_layers = 4
     learning_rate = 1
-    truncated_backprop = 24*7
+    truncated_backprop_num = 24*7
 
     """
     END Parameter Section
@@ -112,7 +120,7 @@ if __name__ == "__main__":
     # Get Target Data
     target_data = train_data.loc[:,'DryBulbFarenheit_' + station_target].values
     target_data = target_data - min_temp
-    target_data = round(target_data)
+    target_data = np.round(target_data)
 
     # Get Normalized Input Data
     for type in measurement_types:
@@ -122,20 +130,20 @@ if __name__ == "__main__":
         minVal = train_data.loc[:,b].min().min()
         input_data = (train_data.values - minVal) / (maxVal - minVal)
 
-    def getFeeder(truncated_backprop, batch_size):
-        total_size = batch_size*truncated_backprop
-        randIndexs = np.random.randint(24,input_data.shape[0]-25,total_size)
+    def getFeeder(truncated_backprop_num, batch_size):
+        total_size = batch_size*truncated_backprop_num
+        randIndexs = np.random.randint(0,input_data.shape[0]-total_size-48,total_size)
         
         if batch_size is 0:
-            inputs = np.zeros((batch_size,truncated_backprop,input_data.shape[2]),dtype=tf.float32)
-            targets = np.zeros((batch_size,truncated_backprop,1),dtype=tf.float32)
+            inputs = np.zeros((batch_size,truncated_backprop_num,input_data.shape[2]),dtype=tf.float32)
+            targets = np.zeros((batch_size,truncated_backprop_num,1),dtype=tf.float32)
             for batch in np.arange(batch_size):
-                selection = np.arange(randIndexs[batch],randIndexs[batch]+truncated_backprop)
+                selection = np.arange(randIndexs[batch],randIndexs[batch]+truncated_backprop_num)
                 inputs[batch,:,:] = input_data[selection]
-                targets[batch,:,1] = target_data[selection],
+                targets[batch,:,1] = target_data[selection+24],
         else: 
-            inputs = input_data[-truncated_backprop:]
-            targets = input_data[-truncated_backprop:]
+            inputs = input_data[-truncated_backprop_num:]
+            targets = input_data[-truncated_backprop_num:]
 
         feed = {
                     'input_batch_PH': inputs,
@@ -147,7 +155,7 @@ if __name__ == "__main__":
     # Build Training Model
     with tf.name_scope('Train'):
         with tf.variable_scope('Model', reuse=False):
-            modelTrain = tempModel(num_of_classes=num_of_classes, isTraining=True, truncated_backprop=truncated_backprop,
+            modelTrain = tempModel(num_of_classes=num_of_classes, isTraining=True, truncated_backprop_num=truncated_backprop_num,
                                    num_rnn_layers=num_rnn_layers, stations=stations)
             global_step = tf.Variable(0, name='global_step', trainable=False)
             train_step = tf.train.AdadeltaOptimizer(learning_rate = learning_rate).minimize(loss=modelTrain.cost_mean, global_step=global_step)
@@ -156,7 +164,7 @@ if __name__ == "__main__":
     # Build Prediction Model
     with tf.name_scope('Pred'):
         with tf.variable_scope('Model', reuse=True):
-            modelPred = tempModel(num_of_classes=num_of_classes, isTraining=False, truncated_backprop=truncated_backprop,
+            modelPred = tempModel(num_of_classes=num_of_classes, isTraining=False, truncated_backprop_num=truncated_backprop_num,
                                   num_rnn_layers=num_rnn_layers, stations=stations)
             tf.summary.histogram('temp_pred_prob', modelPred.temp_pred_prob)
 
@@ -175,7 +183,7 @@ if __name__ == "__main__":
             if sv.should_stop():
                 break
 
-            feed = getFeeder(truncated_backprop, batch_size)   
+            feed = getFeeder(truncated_backprop_num, batch_size)   
 
             _summary, _train_step, _predictions = sess.run(
                 [summary, train_step, modelTrain.temp_pred_prob],
@@ -186,6 +194,6 @@ if __name__ == "__main__":
                 print("Step", step, 'Cost', modelTrain.cost_mean, 'of', num_of_train_steps)
 
         # Use trained model for predictions
-        feed = getFeeder(truncated_backprop=24*30, batch_size=1)
+        feed = getFeeder(truncated_backprop_num=24*30, batch_size=1)
       
         _predictions = sess.run([modelPred.temp_pred_prob], feed_dict=feed)
